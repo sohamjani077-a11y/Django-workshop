c1d2a6e12aee8ae8c16dc833240a37fb01497a30
soham <sohamjani077@gmail.com>
2026-06-19 11:54:04 +0530

    cool

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class student (models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    def __str__(self):
+        return self.name    
+    
+
 # Create your models here.
